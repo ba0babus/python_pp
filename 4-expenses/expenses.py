d0202bb0b6e9d3327625f6a3e9a09fb63085b8ca
Denis <denis.uliankin@gmail.com>
2026-01-21 21:47:04 +0300

    4 dz

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,7 +1,7 @@
-spends = [10, 20, 30, 40, 50]
+spends = [10, 20, 30, 40, 50, 60, 70]
 minimum = min(spends)
 maximum = max(spends)
 total = sum(spends)
 avg = sum(spends) / len(spends)
-result = (minimum, maximum, total, avg)
+result = (minimum, maximum, total)
 print(result)
