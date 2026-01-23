b860add2519af95605652176bf3b571fa376c6f5
Denis <denis.uliankin@gmail.com>
2026-01-23 08:35:01 +0300

    5 dz

--- 4-expenses/expences.py
+++ 4-expenses/expences.py
@@ -1,22 +1,21 @@
-user_input = input("Введите сумму в формате \"10 руб 40 коп\": ")
-user_input = user_input.strip(" ")
-user_input = user_input.lower()
-split_input = user_input.split()
+#user_input = input("Введите сумму в формате \"10 руб 40 коп\": ")
+user_input = "   10 РУБ 4 коп "
+user_input = user_input.strip(" ").lower().split()
+if len(user_input[2]) == 1:
+    user_input[2] = "0" + user_input[2]
 
-if (len(split_input) == 2):
-    if split_input[1] != "руб":
+if (len(user_input) == 2):
+    if user_input[1] != "руб":
         print("Некорректный формат суммы. Не введены \"руб\"")
     else:
-        print(f"{split_input[0]}.00 ₽")
-elif (len(split_input) == 4):
-    if split_input[1] != "руб":
+        print(f"{user_input[0]}.00 ₽")
+elif (len(user_input) == 4):
+    if user_input[1] != "руб":
         print("Некорректный формат суммы. Не введены \"руб\"")
-    elif split_input[3] != "коп":
+    elif user_input[3] != "коп":
         print("Некорректный формат суммы. Не введены \"коп\"")
-    elif len(split_input[2]) != 2:
-        print("Некорректный формат суммы. Не верно введены коп")
     else:
-        print(f"{int(split_input[0])}.{int(split_input[2])} ₽")
+        print(f"{user_input[0]}.{user_input[2]} ₽")
 else:
     print("Некорректный формат суммы. Введены не все значения")
 
