9453c7338d9e104311197ce6e5702a9e2ddbcc08
Denis <denis.uliankin@gmail.com>
2026-01-23 08:47:42 +0300

    5 dz

--- 4-expenses/expences.py
+++ 4-expenses/expences.py
@@ -1,10 +1,10 @@
 user_input = input("Введите сумму в формате \"10 руб 40 коп\": ")
 user_input = user_input.strip(" ").lower().split()
-if len(user_input[2]) == 1:
-    user_input[2] = "0" + user_input[2]
 
 if (len(user_input) == 2):
-    if user_input[1] != "руб":
+    if user_input[0].isdigit() == False:
+        print("Некорректный формат суммы")
+    elif user_input[1] != "руб":
         print("Некорректный формат суммы. Не введены \"руб\"")
     else:
         print(f"{user_input[0]}.00 ₽")
@@ -13,6 +13,8 @@ elif (len(user_input) == 4):
         user_input[2] = "0" + user_input[2]
     if user_input[1] != "руб":
         print("Некорректный формат суммы. Не введены \"руб\"")
+    elif user_input[0].isdigit() == False or user_input[2].isdigit() == False:
+        print("Некорректный формат суммы")
     elif user_input[3] != "коп":
         print("Некорректный формат суммы. Не введены \"коп\"")
     else:
