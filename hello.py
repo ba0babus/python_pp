5862aeab641bd4272e3f936e15021da12bc4384a
Denis <denis.uliankin@gmail.com>
2026-01-24 13:18:21 +0300

    dz 10

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-print(f"Привет, {name}! Тебе {age} лет")
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print(f"Привет, {name}! Тебе {age} лет")
+
+def add_num(a,b):
+    return a + b
